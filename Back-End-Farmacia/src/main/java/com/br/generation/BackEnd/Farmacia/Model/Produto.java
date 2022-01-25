ead984ed172b5df51c4507ff4fdd2605ae9724f1
Fernanda <fernandasoares.reis1@gmail.com>
2022-01-25 17:49:23 -0300

    @fernanda-reis [Criação de model e repositório Produto]

---
+++ Back-End-Farmacia/src/main/java/com/br/generation/BackEnd/Farmacia/Model/Produto.java
@@ -0,0 +1,61 @@
+package com.br.generation.BackEnd.Farmacia.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private Double valor;
+	
+	@NotNull
+	private int validade;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public int getValidade() {
+		return validade;
+	}
+
+	public void setValidade(int validade) {
+		this.validade = validade;
+	}
+	
+}
