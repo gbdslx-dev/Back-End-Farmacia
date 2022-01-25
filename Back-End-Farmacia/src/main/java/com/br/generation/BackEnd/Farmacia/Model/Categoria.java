68dfed0b1d373ab1879d889632a817fdb7f2e2bb
David <daviidccastro@gmail.com>
2022-01-25 16:12:38 -0300

    Adicionado Class dentro da camada Model 'Categoria'

--- Back-End-Farmacia/src/main/java/com/br/generation/BackEnd/Farmacia/Model/Categoria.java
+++ Back-End-Farmacia/src/main/java/com/br/generation/BackEnd/Farmacia/Model/Categoria.java
@@ -1,5 +1,51 @@
 package com.br.generation.BackEnd.Farmacia.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table (name = "tb_categoria")
 public class Categoria {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@NotBlank
+	private String Classificacao;
+	
+	@NotNull
+	@NotBlank
+	private String Fabricante;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getClassificacao() {
+		return Classificacao;
+	}
+
+	public void setClassificacao(String classificacao) {
+		Classificacao = classificacao;
+	}
+
+	public String getFabricante() {
+		return Fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		Fabricante = fabricante;
+	}
+	
 }
